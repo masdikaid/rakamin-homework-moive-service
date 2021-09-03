3cd610a7e6a36ae35a2e46aa4261d2cc9b8535e7
masdika ID <58813839+masdikaid@users.noreply.github.com>
2021-09-03 22:46:44 +0700

    add feature : get | update | delete Movie

--- service/movie.go
+++ service/movie.go
@@ -21,3 +21,28 @@ func (m *Movie) Create() error {
 	}
 	return nil
 }
+
+func (m *Movie) Update() (*Movie, error) {
+	err := databases.MysqlDB.Save(m)
+	if err.Error != nil {
+		return nil, err.Error
+	}
+	return m, nil
+}
+
+func (m *Movie) Delete() error {
+	err := databases.MysqlDB.Delete(m)
+	if err.Error != nil {
+		return err.Error
+	}
+	return nil
+}
+
+func GetMovie(slug string) (*Movie, error) {
+	var m *Movie
+	err := databases.MysqlDB.First(&m, "slug=?", slug)
+	if err.Error != nil {
+		return nil, err.Error
+	}
+	return m, nil
+}
